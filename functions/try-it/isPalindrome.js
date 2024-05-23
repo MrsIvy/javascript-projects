737ef763674c5c55234769f4df624056379ea64b
LaTanya Ivy <latanyaivy@gmail.com>
2024-05-23 16:17:29 -0500

    candidate-testing part 2

--- functions/try-it/isPalindrome.js
+++ functions/try-it/isPalindrome.js
@@ -1,7 +1,13 @@
-function reverse(str) {
-   return str.split('').reverse().join('');
-}
+// function reverse(str) {
+//    return str.split('').reverse().join('');
+// }
 
-function isPalindrome(str) {
-   return reverse(str) === str;
+// function isPalindrome(str) {
+//    return reverse(str) === str;
+// }
+
+function reverse(str) {
+   let lettersArray = str.split('');
+   let reversedLettersArray = lettersArray.reverse();
+   return reversedLettersArray.join('');
 }
--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,3 +1,95 @@
-function sayHello() {
-   console.log("Hello, World!");
+// function sayHello() {
+//    console.log("Hello, World!");
+// }
+
+// function sayHello() {
+//    console.log("Hello, World!");
+// }
+
+// sayHello();
+
+// function printArray(names) {
+//    for (let i = 0; i < names.length; i++) {
+//        console.log(names[i]);
+//    }
+// }
+
+// printArray(["Lena", "James", "Julio"]);
+// console.log("---");
+// printArray(["orange", "apple", "pear"]);
+
+// function sumToN(n) {
+//    let sum = 0;
+//    for (let i = 0; i <= n; i++) {
+//        sum += i;
+//    }
+//    return sum;
+// }
+
+// console.log(sumToN(3));
+
+// function plusTwo(num) {
+//    return num + 2;
+// }
+
+// let a = 2;
+
+// for (let i=0; i < 4; i++) {
+//    a = plusTwo(a);
+// }
+
+// console.log(a);
+
+
+// let message = "Hello, World!";
+
+// function printMessage() {
+//     console.log(message);
+// }
+
+// printMessage();
+// message = "Goodbye";
+// printMessage();
+
+
+// let num = 42;
+
+// function isEven (num) { 
+//     return num % 2 === 0; 
+// }
+
+// console.log(isEven(43));
+
+// function plusTwo(num) {
+//    return num + 2;
+// }
+
+// let a = 2;
+
+// for (let i=0; i < 4; i++) {
+//    a = plusTwo(a);
+// }
+
+// console.log(a);
+
+// function repeater(str) {
+//    let repeated = str + str;
+//    console.log(repeated);
+// }
+
+// repeater('Bob');
+
+// let num = 42;
+
+// function isEven (num) {
+//    return num % 2 === 0;
+// }
+
+// console.log(isEven(43));
+
+function repeater(str) {
+   let repeated = str + str;
+   console.log(repeated);
 }
+
+repeater('Bob');
